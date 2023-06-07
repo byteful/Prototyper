64ef406be662c1f9ff0e3c8c6bedaaf799728259
byteful <info@byteful.me>
2023-06-07 16:00:16 -0500

    Initial commit

---
+++ src/main/java/me/byteful/plugin/prototyper/PrototyperPlugin.java
@@ -0,0 +1,17 @@
+package me.byteful.plugin.prototyper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Prototyper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/byteful/plugin/prototyper/script/Script.java
@@ -0,0 +1,2 @@
+package me.byteful.plugin.prototyper.script;public class Script {
+}
